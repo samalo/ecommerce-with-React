de12a3a6bb23c4efd8297796e8ba51826f00936c
samalo <smhonds@gmail.com>
2020-04-09 02:50:25 +0200

    Updating my app with shop page, collectin item page and pre-review page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components.jsx';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <HomePage/>
+     <Switch>
+     <Route exact path='/' component={HomePage}/>
+     <Route path='/hats' component={ShopPage}/>
+     </Switch>
     </div>
   );
 }
